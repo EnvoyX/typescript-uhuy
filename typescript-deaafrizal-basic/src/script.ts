65f4861b4641a69130fd62e2b4e9c159cb8bc5f2
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-12-12 21:46:04 +0700

    update typescript basic

--- typescript-deaafrizal-basic/src/script.ts
+++ typescript-deaafrizal-basic/src/script.ts
@@ -1,2 +1,27 @@
 console.log("Dea Afrizal");
-console.log("Andi Jafar`");
+console.log("Andi Jafar");
+
+// Explicitly defined type daya
+let myName: string = "Hanif Hafizhan";
+let username: string = "Envoy";
+let isDead: boolean = false;
+let nim: number = 13523666;
+
+// any type variable
+let terserah: any = "serah lu";
+console.log({ terserah });
+terserah = 10;
+console.log({ terserah });
+
+// Implicitly defined type data
+let usia = 20;
+let human = true;
+
+// impossible to do this:
+// usia = "andi"
+// human = "hidup"
+
+console.log(myName);
+console.log(username);
+console.log(isDead);
+console.log(nim);

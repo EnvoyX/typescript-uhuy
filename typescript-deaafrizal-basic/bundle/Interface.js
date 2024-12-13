5ab51705ea56abdbbda7c32a3fe752cb8050915b
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-12-13 22:07:24 +0700

    learned how to extend in Interface on TS

--- typescript-deaafrizal-basic/bundle/Interface.js
+++ typescript-deaafrizal-basic/bundle/Interface.js
@@ -1,5 +1,5 @@
 "use strict";
-function create(processor) {
+function createIntel(processor) {
     // output : [object Object]
     //   console.log(`Sucessfully create processor: ${processor}`);
     console.log(`Sucessfully create processor: ${processor.brand} dengan model ${processor.modelName}`);
@@ -13,7 +13,26 @@ function create(processor) {
     Base Model: ${processor.baseModel}
     Name Model : ${processor.modelName}
     Clock Speed : ${processor.clockSpeed}
+    Core Total : ${processor.coreTotal}
     Turbo Boost Compatbile: ${processor.turboBoost}
+    `);
+}
+function createAMD(processor) {
+    // output : [object Object]
+    //   console.log(`Sucessfully create processor: ${processor}`);
+    console.log(`Sucessfully create processor: ${processor.brand} dengan model ${processor.modelName}`);
+    console.log(`
+    ------------------------------
+    Thank you, ${processor.brand} 🙏
+    -----------------------------
+    You have been sucessfully manufactured new processor dengan
+    detail: 👇
+
+    Base Model: ${processor.baseModel}
+    Name Model : ${processor.modelName}
+    Clock Speed : ${processor.clockSpeed}
+    Core Total : ${processor.coreTotal}
+    Precision Boost Compatbile: ${processor.precisionBoost}
     `);
 }
 const intelCoreI7 = {
@@ -21,7 +40,17 @@ const intelCoreI7 = {
     baseModel: 'Core i7',
     modelName: 'i7-13300K',
     clockSpeed: 4,
+    coreTotal: 6,
     turboBoost: true,
 };
+const amdRyzen5700XD = {
+    brand: 'AMD Ryzen',
+    baseModel: 'AMD Ryzen 5',
+    modelName: '5700XD',
+    clockSpeed: 6,
+    coreTotal: 8,
+    precisionBoost: true,
+};
 // Argument pada fungsi harus bertipe data yang sama dengan parameter (createProcessor)
-create(intelCoreI7);
+createIntel(intelCoreI7);
+createAMD(amdRyzen5700XD);
--- typescript-deaafrizal-basic/src/Interface.ts
+++ typescript-deaafrizal-basic/src/Interface.ts
@@ -5,14 +5,25 @@ interface Processor {
   baseModel: string;
   modelName: string;
   clockSpeed: number;
+  coreTotal: 2 | 4 | 6 | 8 | 12 | 16;
 }
 
 // Interface can be rewritten to create new property such like this:
-interface Processor {
+// interface Processor {
+//   turboBoost: boolean;
+// }
+
+// Or
+
+interface Intel extends Processor {
   turboBoost: boolean;
 }
 
-function create(processor: Processor): void {
+interface AMD extends Processor {
+  precisionBoost: boolean;
+}
+
+function createIntel(processor: Intel): void {
   // output : [object Object]
   //   console.log(`Sucessfully create processor: ${processor}`);
   console.log(
@@ -28,17 +39,49 @@ function create(processor: Processor): void {
     Base Model: ${processor.baseModel}
     Name Model : ${processor.modelName}
     Clock Speed : ${processor.clockSpeed}
+    Core Total : ${processor.coreTotal}
     Turbo Boost Compatbile: ${processor.turboBoost}
     `);
 }
+function createAMD(processor: AMD): void {
+  // output : [object Object]
+  //   console.log(`Sucessfully create processor: ${processor}`);
+  console.log(
+    `Sucessfully create processor: ${processor.brand} dengan model ${processor.modelName}`
+  );
+  console.log(`
+    ------------------------------
+    Thank you, ${processor.brand} 🙏
+    -----------------------------
+    You have been sucessfully manufactured new processor dengan
+    detail: 👇
 
-const intelCoreI7 = {
+    Base Model: ${processor.baseModel}
+    Name Model : ${processor.modelName}
+    Clock Speed : ${processor.clockSpeed}
+    Core Total : ${processor.coreTotal}
+    Precision Boost Compatbile: ${processor.precisionBoost}
+    `);
+}
+
+const intelCoreI7: Intel = {
   brand: 'intel Core i7 ',
   baseModel: 'Core i7',
   modelName: 'i7-13300K',
   clockSpeed: 4,
+  coreTotal: 6,
   turboBoost: true,
 };
 
+const amdRyzen5700XD: AMD = {
+  brand: 'AMD Ryzen',
+  baseModel: 'AMD Ryzen 5',
+  modelName: '5700XD',
+  clockSpeed: 6,
+  coreTotal: 8,
+  precisionBoost: true,
+};
+
 // Argument pada fungsi harus bertipe data yang sama dengan parameter (createProcessor)
-create(intelCoreI7);
+createIntel(intelCoreI7);
+createAMD(amdRyzen5700XD);
